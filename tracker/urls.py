48b63a9de691d6e6220c8dea9353f3cbabdbb9b3
nikita-sychev182 <n.e.sychev@yandex.ru>
2026-03-24 16:18:39 +0300

    Refactor URL routing to use class-based views for item listing and detail. Implement ItemListView and ItemDetailView in views.py.

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from tracker.views import home
+from tracker.views import ItemDetailView, ItemListView
 
 app_name = "tracker"
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", ItemListView.as_view(), name="home"),
+    path("items/", ItemListView.as_view(), name="item_list"),
+    path("items/<int:pk>/", ItemDetailView.as_view(), name="item_detail"),
 ]
 
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,25 @@
-from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+
+from tracker.models import Item
 
 
-def home(request):
-    return HttpResponse("Главная страница Taste Tracker")
+class ItemListView(ListView):
+    model = Item
+    template_name = "tracker/item_list.html"
+    context_object_name = "items"
+
+    def get_queryset(self):
+        queryset = (
+            Item.objects.select_related("user", "category")
+            .order_by("-created_at")
+        )
+        if self.request.user.is_authenticated:
+            return queryset.filter(user=self.request.user)
+        return queryset
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "tracker/item_detail.html"
+    context_object_name = "item"
 
